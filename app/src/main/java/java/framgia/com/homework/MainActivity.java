a5594b21e63187d33ea97b49e83d4dbaf910fc0f
ToanDoan <toandoan.coder@gmail.c>
2017-03-09 19:51:57 +0700

    first_commit

---
+++ app/src/main/java/java/framgia/com/homework/MainActivity.java
@@ -0,0 +1,12 @@
+package java.framgia.com.homework;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
